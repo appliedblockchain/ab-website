4f01ed6536fff46764c59e01c6e344f78b3bd865
ArnaudAB <arnaud@appliedblockchain.com>
2020-04-30 16:29:55 +0100

    refactor: about; style: init. component based styling w/ about;

--- resources/js/tabs.js
+++ resources/js/tabs.js
@@ -54,12 +54,12 @@ $(".event.tab--vertical .tab__nav a").click(function(event) {
 
 // leadership
 
-$(".leadership.tab--vertical .tab__nav a").click(function(event) {
+$(".tab--leadership .tab__nav a").click(function(event) {
 	event.preventDefault();
 	$(this).addClass("nav__button--active");
 	$(this).siblings().removeClass("nav__button--active");
 	var tab = $(this).attr("href");
-	$(".leadership.tab--vertical .tab__item").not(tab).css("display", "none");
+	$(".tab--leadership .tab__item").not(tab).css("display", "none");
 	$(tab).fadeIn(400);
 });
 

820d5973ee199dc20d033aed5aca503fc506aa50
ArnaudAB <arnaud@appliedblockchain.com>
2020-09-09 15:51:43 +0100

    script: filter (fix overlapping bug);

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-
-
 // —————————————————————————————————————————————————————————————————
 // FILTER (FOR BLOG POSTS)
 // —————————————————————————————————————————————————————————————————
@@ -9,17 +7,20 @@ $(document).ready(function() {
 // init Isotope
 var $grid = $('.grid').isotope({
     // options
-  });
-  // filter items on button click
-  $('.js-blog-filter').on( 'click', 'a', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
-  $('.js-blog-filter .tab__button').on('click', function(){
-      $('.js-blog-filter .tab__button').addClass('button--primary-lighten');
-      $(this).removeClass('button--primary-lighten');
-  });
+});
+// filter items on button click
+$('.js-blog-filter').on( 'click', 'a', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+$('.js-blog-filter .tab__button').on('click', function(){
+    $('.js-blog-filter .tab__button').addClass('button--primary-lighten');
+    $(this).removeClass('button--primary-lighten');
+});
 
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
+});
 
 
 // —————————————————————————————————————————————————————————————————

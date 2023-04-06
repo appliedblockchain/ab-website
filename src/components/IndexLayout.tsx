9848435ad90caf904f91f9f5a2c87c5b9dcbd9d8
Victor <vzabrovskiu@gmail.com>
2023-04-06 17:39:54 +0100

    added gtm logic

--- src/components/IndexLayout.tsx
+++ src/components/IndexLayout.tsx
@@ -2,10 +2,17 @@ import Head from 'next/head';
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import TagManager from 'react-gtm-module';
 
 interface IndexLayoutProps {
   children?: ReactNode;
 }
+
+const tagManagerArgs = {
+  gtmId: 'GTM-P69MF67',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 export function IndexLayout({ children }: IndexLayoutProps) {
   return (

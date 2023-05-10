7cfb6bbb3a3435e3896747ea2019ffdf6fc06b9e
Victor <vzabrovskiu@gmail.com>
2023-05-10 18:48:58 +0100

    no test lottie

--- src/components/HomePage/WhatMakesUsDifferentSection.tsx
+++ src/components/HomePage/WhatMakesUsDifferentSection.tsx
@@ -43,82 +43,80 @@ function WhatMakesUsDifferentSection() {
   });
 
   return (
-    <StyledSection item container xs={12}>
-      <Grid item xs={12} className={clsx('flex-centered', 'column')}>
-        <Typography variant="h4" color="secondary">
-          What makes us different?
-        </Typography>
-        <Typography variant="h2" color="primary.contrastText">
-          Our Core technologies
-        </Typography>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        md={6}
-        className={clsx('flex-centered', 'column', classes.texts)}
-      >
-        <Typography
-          variant="h5"
-          color="primary.contrastText"
-          className={classes.title}
+      <StyledSection item container xs={12}>
+        <Grid item xs={12} className={clsx('flex-centered', 'column')}>
+          <Typography variant="h4" color="secondary">
+            What makes us different?
+          </Typography>
+          <Typography variant="h2" color="primary.contrastText">
+            Our Core technologies
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          className={clsx('flex-centered', 'column', classes.texts)}
         >
-          Blockchain based solutions
-        </Typography>
-        <Typography variant="subtitle1" color="grey.500">
-          Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
-          Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
-          Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
-          Paragraph Paragraph Paragraph
-        </Typography>
-      </Grid>
-      <Grid item xs={6}>
-        {Animation}
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        md={6}
-        className={clsx('flex-centered', 'column', classes.texts)}
-      >
-        <Typography
-          variant="h5"
-          color="primary.contrastText"
-          className={classes.title}
+          <Typography
+            variant="h5"
+            color="primary.contrastText"
+            className={classes.title}
+          >
+            Blockchain based solutions
+          </Typography>
+          <Typography variant="subtitle1" color="grey.500">
+            Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
+            Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
+            Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
+            Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>{Animation}</Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          className={clsx('flex-centered', 'column', classes.texts)}
         >
-          Zero-Knowledge Technology
-        </Typography>
-        <Typography variant="subtitle1" color="grey.500">
-          We use ZK Proofs cryptographic techniques to privately verify
-          transactions without revealing the underlying information. Blockchain
-          technology combined with ZKP algorithms is a powerful combination for
-          security enabled protocols.
-        </Typography>
-      </Grid>
-      <Grid item xs={6}></Grid>
-      <Grid
-        item
-        xs={12}
-        md={6}
-        className={clsx('flex-centered', 'column', classes.texts)}
-      >
-        <Typography
-          variant="h5"
-          color="primary.contrastText"
-          className={classes.title}
+          <Typography
+            variant="h5"
+            color="primary.contrastText"
+            className={classes.title}
+          >
+            Zero-Knowledge Technology
+          </Typography>
+          <Typography variant="subtitle1" color="grey.500">
+            We use ZK Proofs cryptographic techniques to privately verify
+            transactions without revealing the underlying information.
+            Blockchain technology combined with ZKP algorithms is a powerful
+            combination for security enabled protocols.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          className={clsx('flex-centered', 'column', classes.texts)}
         >
-          Secure Hardware Enclaves
-        </Typography>
-        <Typography variant="subtitle1" color="grey.500">
-          In addition, we use Intel SGX hardware-based memory encryption to
-          isolate specific application code and data in memory. Intel SGX allows
-          Applied Blockchain to allocate private regions of memory, called
-          enclaves, which are designed to be protected from processes running at
-          higher privilege levels.
-        </Typography>
-      </Grid>
-      <Grid item xs={6}></Grid>
-    </StyledSection>
+          <Typography
+            variant="h5"
+            color="primary.contrastText"
+            className={classes.title}
+          >
+            Secure Hardware Enclaves
+          </Typography>
+          <Typography variant="subtitle1" color="grey.500">
+            In addition, we use Intel SGX hardware-based memory encryption to
+            isolate specific application code and data in memory. Intel SGX
+            allows Applied Blockchain to allocate private regions of memory,
+            called enclaves, which are designed to be protected from processes
+            running at higher privilege levels.
+          </Typography>
+        </Grid>
+        <Grid item xs={6}></Grid>
+      </StyledSection>
   );
 }
 
